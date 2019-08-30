15443673362ee88b4f36873d4492d4c87bdc00fb
amartin <alejandro.martin@henneo.com>
2019-08-30 10:45:42 +0200

    Multiple inputs and outputs

--- rollup.config.js
+++ rollup.config.js
@@ -4,22 +4,35 @@ import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 
 
-export default {
-    input: './src/main.js',
-    output: {
-        file: './build/bundle.min.js',
-        format: 'iife',
-        name: 'bundle',
-        globals: {
-            'lodash': '_',
-        }
+export default [
+    {
+        input: './src/example1.js',
+        output: {
+            file: './build/example1.min.js',
+            format: 'iife',
+            name: 'minutos',
+            globals: { 'lodash': '_' }
+        },
+        plugins: [
+            babel({ exclude: 'node_modules/**' }),
+            resolve(),
+            commonjs(),
+            uglify()
+        ]
     },
-    plugins: [
-        babel({
-            exclude: 'node_modules/**'
-        }),
-        resolve(),
-        commonjs(),
-        uglify()
-    ]
-}
+    {
+        input: './src/example2.js',
+        output: {
+            file: './build/example2.min.js',
+            format: 'iife',
+            name: 'example2',
+            globals: { 'lodash': '_' }
+        },
+        plugins: [
+            babel({ exclude: 'node_modules/**' }),
+            resolve(),
+            commonjs(),
+            uglify()
+        ]
+    },
+]
